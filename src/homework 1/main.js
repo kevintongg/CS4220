597cb12d2a5e5e041b52013fd98958ec85ab6830
Kevin Tong <kcr1230@hotmail.com>
2018-02-11 12:21:41 -0800

    hi

--- src/homework 1/main.js
+++ src/homework 1/main.js
@@ -1,33 +1,40 @@
 function upperCase(s) {
-  let str = '';
+  let temp = '';
   for (let i = 0; i < s.length; i++) {
-    if (s.charCodeAt(i) !== 32 || s.charCodeAt(i) === 46) {
-      if (s.charCodeAt(i) === s.length) {
-        break;
-      }
-      str += String.fromCharCode(s.charCodeAt(i) & 223);
+    let charCode = s.charCodeAt(i);
+    if (charCode >= 97 && charCode <= 122) {
+      charCode -= 32;
     }
+    temp += String.fromCharCode(charCode);
   }
-  return str;
+  return temp;
 }
 
 function lowerCase(s) {
-  let str = '';
+  let temp = '';
   for (let i = 0; i < s.length; i++) {
-    str += String.fromCharCode(s.charCodeAt(i) +32);
+    let charCode = s.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      charCode += 32;
+    }
+    temp += String.fromCharCode(charCode);
   }
-  return str;
+  return temp;
+}
+
+function sentenceCase(s) {
+
 }
 
 function runStringFunctions() {
   let str = 'I watched the storm, so beautiful yet terrific. The face of the moon was in shadow.';
-  
-  let unconditionallyCapitalized = [ 'I', 'Moon', 'Shadow' ];
-  let lowercaseWords = [ 'the', 'of', 'in', 'an' ];
-  
+
+  let unconditionallyCapitalized = ['I', 'Moon', 'Shadow'];
+  let lowercaseWords = ['the', 'of', 'in', 'an'];
+
   console.log('upperCase: ', upperCase(str));
   console.log('lowerCase: ', lowerCase(str));
-  // console.log('sentenceCase: ', sentenceCase(str, unconditionallyCapitalized));
+  console.log('sentenceCase: ', sentenceCase(str, unconditionallyCapitalized));
   // console.log('capitalizedCase: ', capitalizedCase(str));
   // console.log('alternatingCase: ', alternatingCase(str));
   // console.log('titleCase: ', titleCase(str, lowercaseWords));
