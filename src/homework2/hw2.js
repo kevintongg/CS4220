127e0cbbafe9438350d602d6808d06918d7f569a
Kevin Tong <kcr1230@hotmail.com>
2018-02-20 10:53:56 -0800

    hi

--- src/homework2/hw2.js
+++ src/homework2/hw2.js
@@ -4,10 +4,10 @@ class Groups {
   }
 
   get print() {
-    this.list.forEach((item) => {
-      console.log(`${item.name}\nLeader: ${item.leader}`);
-      for (let i = 0; i < item.members.length; i++) {
-        process.stdout.write(`| ${item.members[i]} `);
+    this.list.forEach((e) => {
+      console.log(`${e.name}\nLeader: ${e.leader}`);
+      for (let i = 0; i < e.members.length; i++) {
+        process.stdout.write(`| ${e.members[i]} `);
       }
       console.log('\n');
     });
@@ -15,33 +15,37 @@ class Groups {
   }
 
   addGroup({ name, leader, members = [] }) {
-    this.list.push({ name, leader, members });
+    this.list.push({
+      name,
+      leader,
+      members,
+    });
     return this;
   }
 
   removeGroup(groupName) {
-    this.list.forEach((item, index, object) => {
-      if (item.name.toLowerCase() === groupName) {
-        object.splice(index, 1);
+    this.list.forEach((e, i, obj) => {
+      if (e.name.toLowerCase() === groupName) {
+        obj.splice(i, 1);
       }
     });
     return this;
   }
 
   addMember(groupName, memberName) {
-    this.list.forEach((item) => {
-      if (item.name === groupName) {
-        item.members.push(memberName);
+    this.list.forEach((e) => {
+      if (e.name === groupName) {
+        e.members.push(memberName);
       }
     });
     return this;
   }
 
   removeMember(groupName, memberName) {
-    this.list.forEach((item) => {
-      for (let i = 0; i < item.members.length; i++) {
-        if (item.members[i].toLowerCase() === memberName.toLowerCase()) {
-          item.members.splice(i, 1);
+    this.list.forEach((e) => {
+      for (let i = 0; i < e.members.length; i++) {
+        if (e.members[i].toLowerCase() === memberName.toLowerCase()) {
+          e.members.splice(i, 1);
         }
       }
     });
@@ -50,26 +54,37 @@ class Groups {
 }
 
 const groups = new Groups();
+
 groups.addGroup({
   name: 'Justice League',
   leader: 'Wonder Woman',
-  members: ['Batman', 'Superman', 'Spiderman'],
+  members: [
+    'Batman',
+    'Superman',
+    'Spiderman',
+  ],
 });
+
 groups.addGroup({
   name: 'Avengers',
   leader: 'Iron Man',
-  members: ['Hulk', 'Thor', 'Captain America'],
+  members: [
+    'Hulk',
+    'Thor',
+    'Captain America',
+  ],
 });
-// groups.print;
-//
-// groups.addMember('Justice League', 'Aqua Man');
-// groups.print;
-//
-// groups.removeGroup('avengers');
-// groups.print;
-//
-// groups.removeMember('Justice League', 'spiderMan');
-// groups.print;
+
+groups.print;
+
+groups.addMember('Justice League', 'Aqua Man');
+groups.print;
+
+groups.removeGroup('avengers');
+groups.print;
+
+groups.removeMember('Justice League', 'spiderMan');
+groups.print;
 
 const person = {
   first: 'Elon',
@@ -83,19 +98,19 @@ function displayName() {
   console.log(`${first} ${last}`);
 }
 
-// displayName(person);
+displayName(person);
 
-function combineName(object, key, destination) {
-  const fullName = key.map(k => object[k]).filter(value => value).join(' ');
+function combineName(obj, key, dest) {
+  const fullName = key.map(k => obj[k]).filter(value => value).join(' ');
   delete person.first;
   delete person.last;
 
-  object[destination] = fullName;
+  obj[dest] = fullName;
 }
 
 combineName(person, ['first', 'last'], 'name');
 
-// console.log(person);
+console.log(person);
 
 const people = [
   [{
@@ -116,22 +131,19 @@ const people = [
   }, {
     key: 'company',
     value: 'Apple',
-  }]
+  }],
 ];
 
 function createObject(array) {
-  return {
-    1: {
-      name: array[0][0].value,
-      twitter: array[0][1].value,
-      company: array[0][2].value
-    },
-    2: {
-      name: array[1][0].value,
-      twitter: array[1][1].value,
-      company: array[1][2].value
-    }
-  };
+  const object = {};
+  array.forEach((arr, i) => {
+    const temp = {};
+    arr.forEach((obj) => {
+      temp[obj.key] = obj.value;
+    });
+    object[i + 1] = temp;
+  });
+  return object;
 }
 
 console.log(createObject(people));
