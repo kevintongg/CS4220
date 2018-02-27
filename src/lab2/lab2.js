e4f58b420ed044d757d857a1e250b41623e3d040
Kevin Tong <kcr1230@hotmail.com>
2018-02-27 02:56:41 -0800

    hi

--- src/lab2/lab2.js
+++ src/lab2/lab2.js
@@ -3,14 +3,14 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
-const url = 'https://albertcervantes.com/cs4220/messages.json';
+const string = 'Hello, World!';
+const website = 'https://albertcervantes.com/cs4220/messages.json';
 
 function nonceHashing(message) {
   // For initial hash at 0, 'Hello, World!
   const initialHash = crypto.createHash('sha256');
   initialHash.update(message);
   console.log(`The SHA-256 digest of '${message}' is: [ ${initialHash.digest('hex')} ]`);
-
 
   for (let i = 1; i < Number.POSITIVE_INFINITY; i++) {
     const hash = crypto.createHash('sha256');
@@ -22,8 +22,6 @@ function nonceHashing(message) {
     }
   }
 }
-
-nonceHashing('Hello, World!');
 
 function getJSON(url, callback) {
   https.get(url, (response) => {
@@ -38,34 +36,39 @@ function getJSON(url, callback) {
   });
 }
 
-const json = getJSON(url, (response) => {
-  const string = JSON.stringify(response);
-  const parse = JSON.parse(string);
+function runJSON() {
+  getJSON(website, (response) => {
+    const stringify = JSON.stringify(response);
+    const parse = JSON.parse(stringify);
 
-  const fullPrivateKeyPath = path.resolve('keys', 'private_key.pem');
-  const privateKey = fs.readFileSync(fullPrivateKeyPath, 'utf8');
+    const fullPrivateKeyPath = path.resolve('keys', 'private_key.pem');
+    const privateKey = fs.readFileSync(fullPrivateKeyPath, 'utf8');
 
 
-  console.log('\nGENERATION\n-------');
-  for (const i in Object.keys(parse)) {
-    const sign = crypto.createSign('sha256');
-    sign.update(parse[i].message);
-    const signature = sign.sign(privateKey, 'hex');
-    console.log(`Message: ${parse[i].message}\nSignature: ${signature}\n`);
-  }
+    console.log('\nGENERATION\n-------');
+    for (const i in Object.keys(parse)) {
+      const sign = crypto.createSign('sha256');
+      sign.update(parse[i].message);
+      const signature = sign.sign(privateKey, 'hex');
+      console.log(`Message: ${parse[i].message}\nSignature: ${signature}\n`);
+    }
 
-  const fullPublicKeyPath = path.resolve('keys', 'public_key.pem');
-  const publicKey = fs.readFileSync(fullPublicKeyPath, 'utf8');
-  let isValidSignature;
+    const fullPublicKeyPath = path.resolve('keys', 'public_key.pem');
+    const publicKey = fs.readFileSync(fullPublicKeyPath, 'utf8');
+    let isValidSignature;
 
-  console.log('VERIFICATION\n-------');
-  for (const j in Object.keys(parse)) {
-    const verifySign = crypto.createSign('sha256');
-    const verify = crypto.createVerify('sha256');
-    verifySign.update(parse[j].message);
-    verify.update(parse[j].message);
-    const verifySignature = verifySign.sign(privateKey, 'hex');
-    isValidSignature = verify.verify(publicKey, verifySignature, 'hex');
-    console.log(`Valid: ${isValidSignature} | ${parse[j].message}\nSignature: ${verifySignature}\n`);
-  }
-});
+    console.log('VERIFICATION\n-------');
+    for (const j in Object.keys(parse)) {
+      const verifySign = crypto.createSign('sha256');
+      const verify = crypto.createVerify('sha256');
+      verifySign.update(parse[j].message);
+      verify.update(parse[j].message);
+      const verifySignature = verifySign.sign(privateKey, 'hex');
+      isValidSignature = verify.verify(publicKey, verifySignature, 'hex');
+      console.log(`Valid: ${isValidSignature} | ${parse[j].message}\nSignature: ${verifySignature}\n`);
+    }
+  });
+}
+
+nonceHashing(string);
+runJSON();
