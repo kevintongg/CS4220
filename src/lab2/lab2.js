36cc95764f36f3d2a2ba1ed80510166bbb04974d
Kevin Tong <kcr1230@hotmail.com>
2018-02-27 02:49:27 -0800

    hi

--- src/lab2/lab2.js
+++ src/lab2/lab2.js
@@ -1,44 +1,71 @@
 const crypto = require('crypto');
-const http = require('http');
-const http = require('request');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
-const message = 'Hello, World!';
+const url = 'https://albertcervantes.com/cs4220/messages.json';
 
-// For initial hash at 0, 'Hello, World!
-const initialHash = crypto.createHash('sha256');
-initialHash.update(message);
-console.log(`The SHA-256 digest of '${message}' is: [ ${initialHash.digest('hex')} ]`);
+function nonceHashing(message) {
+  // For initial hash at 0, 'Hello, World!
+  const initialHash = crypto.createHash('sha256');
+  initialHash.update(message);
+  console.log(`The SHA-256 digest of '${message}' is: [ ${initialHash.digest('hex')} ]`);
 
 
-for (let i = 1; i < Number.POSITIVE_INFINITY; i++) {
-  const hash = crypto.createHash('sha256');
-  hash.update(message + i);
-  const digest = hash.digest('hex');
-  console.log(`The SHA-256 digest of '${message + i}' is: [ ${digest} ]`);
-  if (digest.substr(0, 3) === '000') {
-    break;
+  for (let i = 1; i < Number.POSITIVE_INFINITY; i++) {
+    const hash = crypto.createHash('sha256');
+    hash.update(message + i);
+    const digest = hash.digest('hex');
+    console.log(`The SHA-256 digest of '${message + i}' is: [ ${digest} ]`);
+    if (digest.substr(0, 3) === '000') {
+      break;
+    }
   }
 }
 
-// // ----receive function----v
-// function get_json(url, callback) {
-//   http.get(url, (res) => {
-//     let body = '';
-//     res.on('data', (chunk) => {
-//       body += chunk;
-//     });
-//
-//     res.on('end', () => {
-//       const response = JSON.parse(body);
-//       // call function ----v
-//       callback(response);
-//     });
-//   });
-// }
-//
-// // -----------the url---v         ------------the callback---v
-// const mydata = get_json('http://albertcervantes.com/cs4220/messages.json', (resp) => {
-//   console.log(resp);
-// });
-//
-// console.log(mydata);
+nonceHashing('Hello, World!');
+
+function getJSON(url, callback) {
+  https.get(url, (response) => {
+    let body = '';
+    response.on('data', (chunk) => {
+      body += chunk;
+    });
+    response.on('end', () => {
+      const res = JSON.parse(body);
+      callback(res);
+    });
+  });
+}
+
+const json = getJSON(url, (response) => {
+  const string = JSON.stringify(response);
+  const parse = JSON.parse(string);
+
+  const fullPrivateKeyPath = path.resolve('keys', 'private_key.pem');
+  const privateKey = fs.readFileSync(fullPrivateKeyPath, 'utf8');
+
+
+  console.log('\nGENERATION\n-------');
+  for (const i in Object.keys(parse)) {
+    const sign = crypto.createSign('sha256');
+    sign.update(parse[i].message);
+    const signature = sign.sign(privateKey, 'hex');
+    console.log(`Message: ${parse[i].message}\nSignature: ${signature}\n`);
+  }
+
+  const fullPublicKeyPath = path.resolve('keys', 'public_key.pem');
+  const publicKey = fs.readFileSync(fullPublicKeyPath, 'utf8');
+  let isValidSignature;
+
+  console.log('VERIFICATION\n-------');
+  for (const j in Object.keys(parse)) {
+    const verifySign = crypto.createSign('sha256');
+    const verify = crypto.createVerify('sha256');
+    verifySign.update(parse[j].message);
+    verify.update(parse[j].message);
+    const verifySignature = verifySign.sign(privateKey, 'hex');
+    isValidSignature = verify.verify(publicKey, verifySignature, 'hex');
+    console.log(`Valid: ${isValidSignature} | ${parse[j].message}\nSignature: ${verifySignature}\n`);
+  }
+});
