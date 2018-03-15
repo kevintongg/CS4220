e340735a40494d6275612761d2306b7be466fbc8
Kevin Tong <kcr1230@hotmail.com>
2018-03-15 12:18:09 -0700

    hi

--- src/hw3/scripts/app.js
+++ src/hw3/scripts/app.js
@@ -1,48 +1,104 @@
-const https = require('https');
-const axios = require('axios');
-
 const randomJokeURL = 'https://api.chucknorris.io/jokes/random';
 const categoriesURL = 'https://api.chucknorris.io/jokes/categories';
-const categoryList = [];
 
-const SECTIONS = "home, arts, automobiles, books, business, fashion, food, health, insider, magazine, movies, national, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, tmagazine, travel, upshot, world"; // From NYTimes
 
-axios({
-  method: 'get',
-  url: categoriesURL,
-  responseType: 'application/json',
-})
-  .then((response) => {
-    response.data.forEach(e => categoryList.push(e));
-  })
-  .then(() => console.log(categoryList))
-  .catch(error => console.error(error));
+const SECTIONS = 'home, arts, automobiles, books, business, fashion, food, health, insider, magazine, movies, national, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, tmagazine, travel, upshot, world'; // From NYTimes
 
-setTimeout(() => {
-  const chuck = new Vue({
-    el: '#chuck',
-    data: {
-      appName: 'Chuck Norris Jokes',
-      categories: categoryList,
-      category: 'Any',
-      isLoading: false,
-      sections: SECTIONS.split(', '), // create an array of the sections
-      section: 'home', // set default section to 'home'
-    },
-    methods: {
-      getJoke() {
-        this.isLoading = true;
-        const vm = this;
+// Vue.component('dropdown', {
+//   template: '#categories',
+//
+//   mounted: () => {
+//     this.getCategories()
+//       .then(data => this.getCa);
+//   },
+//
+//   data: () => ({
+//     categories: []
+//   }),
+//
+//   methods: {
+//     getCategories: () => {
+//       const d = $.Deferred();
+//     }
+//   }
+// });
 
-        axios.get('https://api.chucknorris.io/jokes/random', {
-          headers: {
-            Accept: 'application/json'
-          }
+const categories = new Vue({
+  el: '#dropdown',
+
+  data: {
+    categories: [],
+  },
+
+  methods: {
+    // getCategories: () => {
+    //   const vm = this;
+    //   axios({
+    //     method: 'get',
+    //     url: categoriesURL,
+    //     responseType: 'application/json',
+    //   })
+    //     .then((response) => {
+    //       response.data.forEach(e => vm.categories.push(e));
+    //     })
+    //     .then(() => console.log(this.categories))
+    //     .catch(error => console.error(error));
+    // }
+    getCategories: () => {
+      const vm = this;
+      const categoryList = [];
+      axios.get(categoriesURL, {
+        headers: {
+          Accept: 'application/json',
+        }
+      })
+        .then((response) => {
+          console.log(response);
+          // vm.categories.push(vm.data);
+          response.data.forEach(e => categoryList.push(e));
         })
-          .then((response) => {
-            vm.isLoading = false;
-          });
-      }
+        .then(() => categoryList)
+        .catch(error => console.error(error));
     }
-  });
-}, 1000);
+  }
+});
+
+// axios({
+//   method: 'get',
+//   url: categoriesURL,
+//   responseType: 'application/json',
+// })
+//   .then((response) => {
+//     response.data.forEach(e => categoryList.push(e));
+//   })
+//   .then(() => console.log(categoryList))
+//   .catch(error => console.error(error));
+//
+// setTimeout(() => {
+//   const chuck = new Vue({
+//     el: '#chuck',
+//     data: {
+//       appName: 'Chuck Norris Jokes',
+//       categories: categoryList,
+//       category: 'Any',
+//       isLoading: false,
+//       sections: SECTIONS.split(', '), // create an array of the sections
+//       section: 'home', // set default section to 'home'
+//     },
+//     methods: {
+//       getJoke() {
+//         this.isLoading = true;
+//         const vm = this;
+//
+//         axios.get('https://api.chucknorris.io/jokes/random', {
+//           headers: {
+//             Accept: 'application/json'
+//           }
+//         })
+//           .then((response) => {
+//             vm.isLoading = false;
+//           });
+//       }
+//     }
+//   });
+// }, 1000);
